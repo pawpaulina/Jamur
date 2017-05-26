c43b8436509727cd3e3b019da1e2319d3ef34f66
pawpaulina <paulina.sudharman.ps@gmail.com>
2017-05-26 23:13:39 +0700

    isi spinner, get data spinner

--- app/src/main/java/com/jamjamur/jamur/IdentifikasiActivity1.java
+++ app/src/main/java/com/jamjamur/jamur/IdentifikasiActivity1.java
@@ -3,13 +3,24 @@ package com.jamjamur.jamur;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 /**
  * Created by Paulina on 5/26/2017.
  */
 public class IdentifikasiActivity1 extends AppCompatActivity {
+    private String[] arrayBentuk;
+    private String[] arrayWarna;
+    private String[] arrayBau;
+
+    private Spinner spinnerBentuk;
+    private Spinner spinnerWarna;
+    private Spinner spinnerBau;
 
     private Button btnLanjut;
     @Override
@@ -17,11 +28,30 @@ public class IdentifikasiActivity1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identifikasi);
 
+        this.arrayBentuk = new String[]{"Bulat", "Payung", "Pipih", "Tiram", "Tudung"};
+        spinnerBentuk = (Spinner) findViewById(R.id.SpinBentuk);
+        ArrayAdapter<String> adapterBentuk = new ArrayAdapter<String>(this, R.layout.spinner_layout, arrayBentuk);
+        spinnerBentuk.setAdapter(adapterBentuk);
+
+        this.arrayWarna = new String[]{"Biru", "Coklat", "Hitam", "Krem", "Kuning", "Oranye", "Merah", "Merah Muda", "Putih", "Violet"};
+        spinnerWarna = (Spinner) findViewById(R.id.SpinWarna);
+        ArrayAdapter<String> adapterWarna = new ArrayAdapter<String>(this, R.layout.spinner_layout, arrayWarna);
+        spinnerWarna.setAdapter(adapterWarna);
+
+        this.arrayBau = new String[]{"Bau", "Tidak Berbau"};
+        spinnerBau = (Spinner) findViewById(R.id.SpinBau);
+        ArrayAdapter<String> adapterBau = new ArrayAdapter<String>(this, R.layout.spinner_layout, arrayBau);
+        spinnerBau.setAdapter(adapterBau);
+
         btnLanjut = (Button) findViewById(R.id.btnLanjut);
     }
 
     public void btnLanjut(View v)
     {
+        String bentuk = spinnerBentuk.getSelectedItem().toString();
+        String warna = spinnerWarna.getSelectedItem().toString();
+        String bau = spinnerBau.getSelectedItem().toString();
+
         Intent i = new Intent(getApplicationContext(), IdentifikasiActivity2.class);
         startActivity(i);
     }
--- app/src/main/java/com/jamjamur/jamur/IdentifikasiActivity2.java
+++ app/src/main/java/com/jamjamur/jamur/IdentifikasiActivity2.java
@@ -1,18 +1,56 @@
 package com.jamjamur.jamur;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * Created by Paulina on 5/26/2017.
  */
 public class IdentifikasiActivity2 extends AppCompatActivity {
+    private String[] arrayCincin;
+    private String[] arrayLendir;
+    private String[] arrayHabitat;
+
+    private Spinner spinnerCincin;
+    private Spinner spinnerLendir;
+    private Spinner spinnerHabitat;
+
     private Button btnLanjut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_identifikasi2);
+
+        this.arrayCincin = new String[]{"Ada", "Tidak Ada"};
+        spinnerCincin = (Spinner) findViewById(R.id.SpinCincin);
+        ArrayAdapter<String> adapterCincin = new ArrayAdapter<String>(this, R.layout.spinner_layout, arrayCincin);
+        spinnerCincin.setAdapter(adapterCincin);
+
+        this.arrayLendir = new String[]{"Ya", "Tidak"};
+        spinnerLendir = (Spinner) findViewById(R.id.SpinLendir);
+        ArrayAdapter<String> adapterLendir = new ArrayAdapter<String>(this, R.layout.spinner_layout, arrayLendir);
+        spinnerLendir.setAdapter(adapterLendir);
+
+        this.arrayHabitat = new String[]{"Jerami", "Kayu Mati"};
+        spinnerHabitat = (Spinner) findViewById(R.id.SpinHabitat);
+        ArrayAdapter<String> adapterHabitat = new ArrayAdapter<String>(this, R.layout.spinner_layout, arrayHabitat);
+        spinnerHabitat.setAdapter(adapterHabitat);
+
         btnLanjut = (Button) findViewById(R.id.btnLanjut2);
+    }
+
+    public void btnLanjut2(View v)
+    {
+        String cincin = spinnerCincin.getSelectedItem().toString();
+        String lendir = spinnerLendir.getSelectedItem().toString();
+        String habitat = spinnerHabitat.getSelectedItem().toString();
+
+//        Intent i = new Intent(getApplicationContext(), IdentifikasiActivity2.class);
+//        startActivity(i);
     }
 }
